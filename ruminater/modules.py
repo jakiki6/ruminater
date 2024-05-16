0d5902a6625518c0993157b2a4547c5a0f6797ef
Jakob Kirsch <jakob.kirsch@web.de>
2024-05-16 23:58:36 +0200

    expand zip module

--- ruminater/modules.py
+++ ruminater/modules.py
@@ -101,10 +101,21 @@ class ZipModule(module.RuminaterModule):
 
         files = []
         for fileinfo in zf.infolist():
-            print(fileinfo, dir(fileinfo))
-
             file = {}
             file["name"] = fileinfo.filename
+            file["date"] = fileinfo.date_time
+            file["compression-type"] = fileinfo.compress_type
+            file["comment"] = fileinfo.comment.decode("utf-8")
+            file["extra"] = fileinfo.comment.hex()
+            file["create-system"] = fileinfo.create_system
+            file["create-version"] = fileinfo.create_version
+            file["extract-version"] = fileinfo.extract_version
+            file["flag-bits"] = fileinfo.flag_bits
+            file["volume"] = fileinfo.volume
+            file["internal-attr"] = fileinfo.internal_attr
+            file["external-attr"] = fileinfo.external_attr
+            file["compress-size"] = fileinfo.compress_size
+
             file["content"] = chew(zf.open(fileinfo.filename, "r"))
 
             files.append(file)
