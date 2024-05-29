7e64e84643feda4caab7a9cc94cb04c49fca179e
Laura Kirsch <laurakirsch240406@gmail.com>
2024-05-30 00:26:31 +0200

    add preliminary support for mp4

--- ruminater/modules.py
+++ ruminater/modules.py
@@ -123,3 +123,22 @@ class ZipModule(module.RuminaterModule):
         return {"type": "zip", "comment": zf.comment.decode("utf-8"), "files": files}
 
 mappings["Zip archive data"] = ZipModule
+
+try:
+    from pymp4.parser import Box
+
+    class Mp4Module(module.RuminaterModule):
+        def chew(self):
+            ftyp = dict(Box.parse(self.blob.read()))
+            file = {}
+            file["type"] = "mp4"
+            file["mp4-type"] = ftyp["type"].decode("utf-8")
+            file["major-brand"] = ftyp["major_brand"].decode("utf-8")
+            file["minor-version"] = ftyp["minor_version"]
+            file["compatible-brands"] = [x.decode("utf-8") for x in ftyp["compatible_brands"]]
+
+            return file
+
+    mappings["ISO Media"] = Mp4Module
+except:
+    print("pymp4 not installed, skipping MP4 parsing")
