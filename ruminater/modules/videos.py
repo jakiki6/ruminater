756ebbbdf5403c59bb8e294c286a80677f0f2d70
Jakob Kirsch <jakob.kirsch@web.de>
2025-07-11 21:34:52 +0200

    add small mp4 parsing and add buffer helper

--- ruminater/modules/videos.py
+++ ruminater/modules/videos.py
@@ -1,21 +1,51 @@
 from . import mappings, chew
 from .. import module
 
-try:
-    from pymp4.parser import Box
+class Mp4Module(module.RuminaterModule):
+    def chew(self):
+        file = {}
 
-    class Mp4Module(module.RuminaterModule):
-        def chew(self):
-            ftyp = dict(Box.parse(self.blob.read()))
-            file = {}
-            file["type"] = "mp4"
-            file["mp4-type"] = ftyp["type"].decode("utf-8")
-            file["major-brand"] = ftyp["major_brand"].decode("utf-8")
-            file["minor-version"] = ftyp["minor_version"]
-            file["compatible-brands"] = [x.decode("utf-8") for x in ftyp["compatible_brands"]]
+        file["atoms"] = []
+        while not self.blob.isend():
+            file["atoms"].append(self.read_atom())
 
-            return file
+        return file
 
-    mappings["^ISO Media.*$"] = Mp4Module
-except ModuleNotFoundError:
-    print("pymp4 not installed, skipping MP4 parsing")
+    def read_atom(self):
+        offset = self.blob.tell()
+
+        length = int.from_bytes(self.blob.read(4), "big")
+        if length == 0:
+            pos = self.blob.tell()
+            self.blob.seek(0, 2)
+            length = self.blob.tell()
+            self.blob.seek(pos)
+        elif length == 1:
+            length = int.from_bytes(self.blob.read(8), "big")
+
+        typ = self.blob.read(4).decode("utf-8")
+        length -= 8
+
+        atom = {
+            "type": typ,
+            "offset": offset,
+            "length" : length,
+            "data": {}
+        }
+
+        if typ == "ftyp":
+            atom["data"]["major_brand"] = self.blob.read(4).decode()
+            atom["data"]["minor_version"] = int.from_bytes(self.blob.read(4), "big")
+            atom["data"]["compatible_brands"] = []
+
+            length -= 8
+            while length:
+                atom["data"]["compatible_brands"].append(self.blob.read(4).decode())
+                length -= 4
+
+        else:
+            self.blob.skip(length)
+
+        return atom
+
+mappings["^ISO Media.*$"] = Mp4Module
