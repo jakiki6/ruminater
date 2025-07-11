9b4265140649d02faaa02b7798ceb15c368781f2
Laura Kirsch <laurakirsch240406@gmail.com>
2025-07-11 23:11:32 +0200

    add other mp4 stuff

--- ruminater/modules/videos.py
+++ ruminater/modules/videos.py
@@ -9,6 +9,14 @@ def mp4_time_to_iso(mp4_time):
     dt = mp4_epoch + timedelta(seconds=mp4_time)
     return dt.isoformat()
 
+def mp4_decode_mdhd_language(lang_bytes):
+    lang_code = int.from_bytes(lang_bytes, byteorder="big") & 0x7fff
+
+    c1 = ((lang_code >> 10) & 0x1F) + 0x60
+    c2 = ((lang_code >> 5)  & 0x1F) + 0x60
+    c3 = ( lang_code        & 0x1F) + 0x60
+
+    return chr(c1) + chr(c2) + chr(c3)
 
 class Mp4Module(module.RuminaterModule):
     def chew(self):
@@ -53,7 +61,7 @@ class Mp4Module(module.RuminaterModule):
             while length > 0:
                 atom["data"]["compatible_brands"].append(self.blob.read(4).decode())
                 length -= 4
-        elif typ in ("moov", "trak"):
+        elif typ in ("moov", "trak", "mdia"):
             atom["data"]["atoms"] = []
             while length > 0:
                 data = self.read_atom()
@@ -87,6 +95,8 @@ class Mp4Module(module.RuminaterModule):
             if version in (0, 1):
                 atom["data"]["creation_time"] = mp4_time_to_iso(creation_time)
                 atom["data"]["modification_time"] = mp4_time_to_iso(modification_time)
+                atom["data"]["timescale"] = timescale
+                atom["data"]["duration"] = duration
 
                 atom["data"]["rate"] = int.from_bytes(self.blob.read(4), "big") / 65536
                 atom["data"]["volume"] = int.from_bytes(self.blob.read(2), "big") / 256
@@ -144,6 +154,69 @@ class Mp4Module(module.RuminaterModule):
                 atom["data"]["height"] = int.from_bytes(self.blob.read(4), "big") / 65536
 
                 length -= 60
+
+            self.blob.skip(length)
+        elif typ == "edts":
+            atom["data"] = self.read_atom()
+        elif typ == "elst":
+            version = self.blob.read(1)[0]
+            atom["data"]["version"] = version
+            atom["data"]["flags"] = int.from_bytes(self.blob.read(3), "big")
+            atom["data"]["entries"] = []
+            count = int.from_bytes(self.blob.read(4), "big")
+            atom["data"]["count"] = count
+            length -= 8
+
+            for i in range(0, count):
+                if version == 0:
+                    segment_duration = int.from_bytes(self.blob.read(4), "big")
+                    media_time = int.from_bytes(self.blob.read(4), "big")
+                    length -= 8
+                elif version == 1:
+                    segment_duration = int.from_bytes(self.blob.read(8), "big")
+                    media_time = int.from_bytes(self.blob.read(8), "big")
+                    length -= 16
+
+                if version in (0, 1):
+                    entry = {}
+                    entry["segment_duration"] = segment_duration
+                    entry["media_time"] = media_time
+                    entry["media_rate_integer"] = int.from_bytes(self.blob.read(2), "big")
+                    entry["media_rate_fraction"] = int.from_bytes(self.blob.read(2), "big")
+
+                    length -= 4
+
+                    atom["data"]["entries"].append(entry)
+
+            self.blob.skip(length)
+        elif typ == "mdhd":
+            version = self.blob.read(1)[0]
+            atom["data"]["version"] = version
+            atom["data"]["flags"] = int.from_bytes(self.blob.read(3), "big")
+            length -= 4
+
+            if version == 0:
+                creation_time = int.from_bytes(self.blob.read(4), "big")
+                modification_time = int.from_bytes(self.blob.read(4), "big")
+                timescale = int.from_bytes(self.blob.read(4), "big")
+                duration = int.from_bytes(self.blob.read(4), "big")
+                length -= 16
+            elif version == 1:
+                creation_time = int.from_bytes(self.blob.read(8), "big")
+                modification_time = int.from_bytes(self.blob.read(8), "big")
+                timescale = int.from_bytes(self.blob.read(4), "big")
+                duration = int.from_bytes(self.blob.read(8), "big")
+                length -= 28
+
+            if version in (0, 1):
+                atom["data"]["creation_time"] = mp4_time_to_iso(creation_time)
+                atom["data"]["modification_time"] = mp4_time_to_iso(modification_time)
+                atom["data"]["timescale"] = timescale
+                atom["data"]["duration"] = duration
+
+                atom["data"]["language"] = mp4_decode_mdhd_language(self.blob.read(2))
+                atom["data"]["pre_defined"] = self.blob.read(2).hex()
+                length -= 4
 
             self.blob.skip(length)
         else:
