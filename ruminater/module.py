2de64615dfbb46f75fd42a8ee7b2187edcad9165
Jakob Kirsch <jakob.kirsch@web.de>
2025-07-13 21:27:17 +0200

    refactor stuff

--- ruminater/module.py
+++ ruminater/module.py
@@ -1,8 +1,16 @@
 import json
 
+modules = []
+
+def register(cls):
+    modules.append(cls)
+
 class RuminaterModule(object):
     def __init__(self, buf):
         self.buf = buf
+
+    def identify(buf):
+        return False
 
     def chew(self):
         pass
