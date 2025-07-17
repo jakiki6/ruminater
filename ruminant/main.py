27751d23c68952a71494f7f2f8ca5863eff1fb5c
Laura Kirsch <laurakirsch240406@gmail.com>
2025-07-17 04:34:36 +0200

    added no argument mode and - handling

--- ruminant/main.py
+++ ruminant/main.py
@@ -4,9 +4,11 @@ import json
 
 
 def main():
-    if len(sys.argv) != 2:
-        print(sys.argv[0], "<file>")
-        exit(1)
+    if len(sys.argv) < 2 or sys.argv[1] == "-":
+        with open("/dev/stdin", "rb") as f:
+            content = f.read()
 
-    with open(sys.argv[1], "rb") as f:
-        print(json.dumps(modules.chew(f), indent=2))
+        print(json.dumps(modules.chew(content), indent=2))
+    else:
+        with open(sys.argv[1], "rb") as f:
+            print(json.dumps(modules.chew(f), indent=2))
