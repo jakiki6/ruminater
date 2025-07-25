0660ba9310ac4f04878e922e1b5a4169665a3c64
Jakob Kirsch <jakob.kirsch@web.de>
2025-07-25 21:52:11 +0200

    more stuff

--- ruminant/modules/fonts.py
+++ ruminant/modules/fonts.py
@@ -5,13 +5,13 @@ from .. import module
 class TrueTypeModule(module.RuminantModule):
 
     def identify(buf):
-        return buf.peek(5) == b"\x00\x01\x00\x00\x00" or buf.peek(4) == b"OTTO"
+        return buf.peek(5) in (b"\x00\x01\x00\x00\x00", b"OTTO\x00")
 
     def chew(self):
         meta = {}
         meta["type"] = "truetype"
 
-        self.buf.skip(4 if self.buf.pu8() else 5)
+        self.buf.skip(4)
 
         num_tables = self.buf.ru16()
         meta["table-count"] = num_tables
@@ -23,11 +23,25 @@ class TrueTypeModule(module.RuminantModule):
         for i in range(0, num_tables):
             table = {}
 
-            table["tag"] = self.buf.rs(4)
+            table["tag"] = self.buf.rs(4, "latin-1")
             table["checksum"] = self.buf.rh(4)
             table["offset"] = self.buf.ru32()
             table["length"] = self.buf.ru32()
 
+            with self.buf:
+                self.buf.seek(table["offset"])
+                self.buf.setunit(table["length"])
+
+                table["data"] = {}
+                match table["tag"]:
+                    case _:
+                        table["unknown"] = True
+
             meta["tables"].append(table)
+
+        for table in meta["tables"]:
+            if table["offset"] + table["length"] > self.buf.tell():
+                self.buf.seek(table["offset"])
+                self.buf.skip(table["length"] + 1)
 
         return meta
